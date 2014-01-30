8e2bf90d78a69dc9f44eecb17d9342e6bb901b3f
kubasub <kubasub12@gmail.com>
2014-01-29 23:19:18 -0500

    created layout files. Colors, icons and button styling to be added.

--- android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/HotseatGameActivity.java
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/HotseatGameActivity.java
@@ -1,6 +1,7 @@
 package ca.brocku.chinesecheckers;
 
 import android.app.Activity;
+import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,13 +9,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Build;
 
-public class MainActivity extends Activity {
+public class HotseatGameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hotseat_game);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -28,7 +30,7 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.hotseat_game, menu);
         return true;
     }
 
@@ -37,9 +39,9 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -55,7 +57,7 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_hotseat_game, container, false);
             return rootView;
         }
     }
