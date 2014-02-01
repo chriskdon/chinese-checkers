894acf9a1b1cbc044e466020b7fd2ce751fce596
Chris <chriskellendonk@gmail.com>
2014-02-01 12:22:58 -0500

    Added pieces and players so that the game board can be worked on.

---
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/Piece.java
@@ -0,0 +1,16 @@
+package ca.brocku.chinesecheckers.gameboard;
+
+import ca.brocku.chinesecheckers.gamestate.Player;
+
+/**
+ * A single piece on the board that is owned
+ * by a Player.
+ *
+ * Author: Chris Kellendonk
+ * Student #: 4810800
+ * Date: 2/1/2014
+ */
+public interface Piece {
+    public Position getPosition();
+    public Player getPlayer();
+}
