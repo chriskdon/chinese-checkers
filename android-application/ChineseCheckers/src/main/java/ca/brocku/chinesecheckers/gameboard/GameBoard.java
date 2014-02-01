93b242d22435a4ecafd06ad0e7114eb30df6346e
Chris <chriskellendonk@gmail.com>
2014-02-01 13:02:31 -0500

    Added game board interface.

---
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/GameBoard.java
@@ -0,0 +1,46 @@
+package ca.brocku.chinesecheckers.gameboard;
+
+/**
+ * The representation of a game board and what it can do.
+ *
+ * Author: Chris Kellendonk
+ * Student #: 4810800
+ * Date: 2/1/2014
+ */
+public interface GameBoard {
+    /**
+     * Move a piece from one position to another.
+     *
+     * @param piece The piece to move.
+     * @param to    The new position of the piece.
+     */
+    public void movePiece(Piece piece, Position to);
+
+    /**
+     * Get the piece that is at a position on the board.
+     *
+     * @param at    The position the piece is at.
+     *
+     * @return      The piece that was at the position specified.
+     */
+    public Piece getPiece(Position at);
+
+    /**
+     * A list of valid positions that the specified piece could move to.
+     *
+     * @param forPiece  The piece to check positions for.
+     *
+     * @return          The list of positions the piece can move to. Or <code>null</code> if there
+     *                  is nowhere to move.
+     */
+    public Position[] getPossibleMoves(Piece forPiece);
+
+    /**
+     * Check if a move is valid for a specified piece.
+     *
+     * @param piece The piece making the move.
+     * @param to    The position the piece is trying to move to.
+     * @return      True if the move is valid, false otherwise.
+     */
+    public boolean isValidMove(Piece piece, Position to);
+}
