6464fb62baeb7677044503cf311c21b970d1ab80
Chris <chriskellendonk@gmail.com>
2014-02-02 12:10:44 -0500

    Updated game board interface (to abstract class)

--- android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/GameBoard.java
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/GameBoard.java
@@ -7,14 +7,21 @@ package ca.brocku.chinesecheckers.gameboard;
  * Student #: 4810800
  * Date: 2/1/2014
  */
-public interface GameBoard {
+public abstract class GameBoard {
+    /**
+     * The number of available positions in each row.
+     */
+    protected final int[] ROW_POSITION_COUNT = {1, 2, 3, 4, 13, 12, 11, 10,
+            9,
+            10, 11, 12, 13, 4, 3, 2, 1};
+
     /**
      * Move a piece from one position to another.
      *
      * @param piece The piece to move.
      * @param to    The new position of the piece.
      */
-    public void movePiece(Piece piece, Position to);
+    public abstract void movePiece(Piece piece, Position to);
 
     /**
      * Get the piece that is at a position on the board.
@@ -23,7 +30,7 @@ public interface GameBoard {
      *
      * @return      The piece that was at the position specified.
      */
-    public Piece getPiece(Position at);
+    public abstract Piece getPiece(Position at);
 
     /**
      * A list of valid positions that the specified piece could move to.
@@ -33,7 +40,7 @@ public interface GameBoard {
      * @return          The list of positions the piece can move to. Or <code>null</code> if there
      *                  is nowhere to move.
      */
-    public Position[] getPossibleMoves(Piece forPiece);
+    public abstract Position[] getPossibleMoves(Piece forPiece);
 
     /**
      * Check if a move is valid for a specified piece.
@@ -42,5 +49,5 @@ public interface GameBoard {
      * @param to    The position the piece is trying to move to.
      * @return      True if the move is valid, false otherwise.
      */
-    public boolean isValidMove(Piece piece, Position to);
+    public abstract boolean isValidMove(Piece piece, Position to);
 }
