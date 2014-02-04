28bb67905591b6a41b0dc6c94295890a260fee84
curtesmith <curt.e.smith@gmail.com>
2014-02-03 23:34:52 -0500

    removed bad test and so the build should run the single test and not break

--- android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/TestableTest.java
+++ android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/TestableTest.java
@@ -18,21 +18,8 @@ public class TestableTest extends TestCase {
     }
 
 
-    /*
-     * this test should pass
-     */
     public void testIsTrueShouldBeTrue() throws Exception {
         Testable sut = new Testable();
         assertTrue("testIsTrueShouldBeTrue failed: isTrue did not return true", sut.isTrue());
-    }
-
-
-    /*
-     * this test should fail.
-     * it was written to observe output created for a failing test
-     */
-    public void testIsTrueShouldBeFalse() throws Exception {
-        Testable sut = new Testable();
-        assertFalse("testIsTrueShouldBeFalse failed: isTrue did not return false", sut.isTrue());
     }
 }
