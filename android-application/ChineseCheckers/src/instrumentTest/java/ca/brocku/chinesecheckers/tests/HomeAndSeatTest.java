dd52ac9c9c4469d22e2f1f016ba6b58a277d92e8
SaajidM <saajid.mohammed@hotmail.com>
2014-02-04 16:37:48 -0500

    Update TestPlan And Created First Integration Test

--- android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/HomeAndSeatTest.java
+++ android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/HomeAndSeatTest.java
@@ -1,32 +1,60 @@
 package ca.brocku.chinesecheckers.tests;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
+
+import ca.brocku.chinesecheckers.HotseatConfigurationActivity;
 import ca.brocku.chinesecheckers.MainActivity;
+import ca.brocku.chinesecheckers.R;
 
 /**
-* Created by Main on 2/3/14.
-*/
+ * Created by Main on 2/3/14.
+ */
 
 public class HomeAndSeatTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private MainActivity thisTest;
-
-    public HomeAndSeatTest(){
+    private Activity curAct;
+    private Instrumentation curInstruments;
+    private Instrumentation.ActivityMonitor monitor;
+    public HomeAndSeatTest() {
         super(MainActivity.class);
     }
 
-    protected void setUp() throws Exception{
+    protected void setUp() throws Exception {
         super.setUp();
         setActivityInitialTouchMode(false);
-        thisTest = getActivity();
+        curAct = (MainActivity)getActivity();
+        curInstruments = getInstrumentation();
     }
 
-    public void testPreConditions(){
-        assertEquals(1,1);
+    public void testPreConditions() {
+        assertNotNull("MainActivity Not Started", curAct);
+//        new MainActivityUnitTest();
     }
 
-    public void testIt(){
-        assertEquals(true,false);
+    public void testButton() {
+        final Button hotseatConfigurationActivityButton = (Button) curAct.findViewById(R.id.hotseatConfigurationActivityButton);
+        monitor = curInstruments.addMonitor(HotseatConfigurationActivity.class.getName(),null,false);
+        assertTrue("hotseatConfigurationActivityButton Did Not Respond To Click",hotseatConfigurationActivityButton.isClickable());
+        TouchUtils.clickView(this, hotseatConfigurationActivityButton);
+        tTransitionToConfigAct();
+//        curAct.runOnUiThread(
+//                new Runnable() {
+//                    public void run() {
+//                        assertNotNull("hotseatConfigurationActivityButton Not Available", hotseatConfigurationActivityButton);
+//                        assertTrue("hotseatConfigurationActivityButton Did Not Respond To Click ",hotseatConfigurationActivityButton.performClick());
+//                    }
+//                }
+//        );
+    }
+
+    public void tTransitionToConfigAct(){
+        curAct = getInstrumentation().waitForMonitorWithTimeout(monitor,1);
+        assertNotNull("Transition to HotseatConfigurationActivity Failed",curAct);
+//        new HotseatConfigurationActivityUnitTest();
     }
 
 }
---
+++ android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/HomeSeatAndGameTest.java
@@ -0,0 +1,14 @@
+package ca.brocku.chinesecheckers.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import ca.brocku.chinesecheckers.MainActivity;
+
+/**
+ * Created by Main on 2/4/14.
+ */
+public class HomeSeatAndGameTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public HomeSeatAndGameTest() {
+        super(MainActivity.class);
+    }
+}
