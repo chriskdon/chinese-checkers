e7757e42cc56e066056c2b909cd9cf8c1e1912ca
SaajidM <saajid.mohammed@hotmail.com>
2014-02-03 23:25:59 -0500

    Partial initialization of testing

---
+++ android-application/ChineseCheckers/src/instrumentTest/java/HomeAndSeatTest.java
@@ -0,0 +1,22 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+import ca.brocku.chinesecheckers.MainActivity;
+
+/**
+ * Created by Main on 2/3/14.
+ */
+
+public class HomeAndSeatTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity thisTest;
+
+    public HomeAndSeatTest(){
+        super(MainActivity.class);
+    }
+    protected void setUp() throws Exception{
+        super.setUp();
+        setActivityInitialTouchMode(false);
+        thisTest = getActivity();
+    }
+}
+initializiation
