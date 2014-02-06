2e088b97499cf9df3787d397ebf00ca688bd3855
Peter Pobojewski <peterpobojewski@gmail.com>
2014-02-05 23:43:15 -0500

    More functions implemented

--- android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/CcGameBoard.java
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/gameboard/CcGameBoard.java
@@ -8,6 +8,10 @@ public class CcGameBoard implements GameBoard{
      * The number of available positions in each row.
      */
     public static final int[] ROW_POSITION_COUNT = {1, 2, 3, 4, 13, 12, 11, 10, 9, 10, 11, 12, 13, 4, 3, 2, 1};
+    /**
+     * Total number of spaces on the board
+     */
+    public static final int TOTAL_PIECE_COUNT = 121;
     GridPiece[][] board;
 
     public CcGameBoard() {
@@ -18,16 +22,18 @@ public class CcGameBoard implements GameBoard{
     }
 
     /**
-     * Total number of spaces on the board
-     */
-    public static final int TOTAL_PIECE_COUNT = 121;
-
-    /**
      * Return all the pieces that are on the board in no specific order.
      *
      * @return  All the pieces on the board.
      */
-    public Piece[] getAllPiece();
+    public GridPiece[] getAllPiece() {
+        GridPiece[] allPieces = new GridPiece[60];
+        for(int i=0; i<board.length;i++) {
+            for(int j=0; j<board[i].length; j++) {
+
+            }
+        }
+    }
 
     /**
      * Move a piece from one position to another.
@@ -35,7 +41,7 @@ public class CcGameBoard implements GameBoard{
      * @param piece The piece to move.
      * @param to    The new position of the piece.
      */
-    public void movePiece(Piece piece, Position to);
+    public void movePiece(GridPiece piece, GridPosition to);
 
     /**
      * Returns all the pieces on the board in no specified order.
@@ -43,7 +49,7 @@ public class CcGameBoard implements GameBoard{
      *
      * @return All the pieces.
      */
-    public Piece[] getAllPieces();
+    public GridPiece[] getAllPieces();
 
     /**
      * Get the piece that is at a position on the board.
@@ -52,7 +58,7 @@ public class CcGameBoard implements GameBoard{
      *
      * @return      The piece that was at the position specified.
      */
-    public Piece getPiece(Position at);
+    public GridPiece getPiece(Position at);
 
     /**
      * A list of valid positions that the specified piece could move to.
@@ -62,7 +68,9 @@ public class CcGameBoard implements GameBoard{
      * @return          The list of positions the piece can move to. Or <code>null</code> if there
      *                  is nowhere to move.
      */
-    public Position[] getPossibleMoves(Piece forPiece);
+    public GridPosition[] getPossibleMoves(GridPiece forPiece) {
+
+    }
 
     /**
      * Check if a move is valid for a specified piece.
@@ -71,5 +79,29 @@ public class CcGameBoard implements GameBoard{
      * @param to    The position the piece is trying to move to.
      * @return      True if the move is valid, false otherwise.
      */
-    public boolean isValidMove(Piece piece, Position to);
+    public boolean isValidMove(GridPiece piece, GridPosition to) {
+        int row = to.getRow();
+        int index = to.getIndex();
+        if(isOccupied(to)) {
+            return false;
+        }
+        try {
+            board[row][index]=piece;
+            board[row][index]=null;
+            return true;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+    private boolean isOccupied (GridPosition at) {
+        int row = at.getRow();
+        int index = at.getIndex();
+        if(board[row][index]!=null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
