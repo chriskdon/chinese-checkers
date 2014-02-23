5b74c49b8e39f2ac718c0d52f4a935dd6aceb712
Chris <chriskellendonk@gmail.com>
2014-02-22 21:02:50 -0500

    Added unit tests for UiEngine components.

--- android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/uiengine/PiecePositionSystemTest.java
+++ android-application/ChineseCheckers/src/instrumentTest/java/ca/brocku/chinesecheckers/tests/uiengine/PiecePositionSystemTest.java
@@ -2,6 +2,8 @@ package ca.brocku.chinesecheckers.tests.uiengine;
 
 import junit.framework.TestCase;
 
+import ca.brocku.chinesecheckers.gameboard.Position;
+import ca.brocku.chinesecheckers.uiengine.PieceDrawingDetails;
 import ca.brocku.chinesecheckers.uiengine.PiecePositionSystem;
 
 /**
@@ -14,15 +16,61 @@ public class PiecePositionSystemTest extends TestCase {
         return new PiecePositionSystem(1200, 1594);
     }
 
+    private Position createPosition(final int row, final int index) {
+        return new Position() {
+            @Override
+            public int getRow() {
+                return row;
+            }
+
+            @Override
+            public int getIndex() {
+                return index;
+            }
+        };
+    }
+
     /**
-     * Make sure the position details can't be modified.
+     * Make sure the position details map can't be modified.
      */
-    public void testUnmodifiable() {
-        PiecePositionSystem pps = init();
+    public void testUnmodifiablePositionMapping() {
+       PiecePositionSystem pps = init();
 
        try {
-           pps.getPositionDetails().clear();
+           pps.getPositionDetailsMapping().clear();
            assertEquals("No Exception was thrown when trying to modify list,", 1, 1);
        } catch (UnsupportedOperationException ex) {}
+    }
+
+    /**
+     * Make sure the position details list can't be modified.
+     */
+    public void testUnmodifiablePositionList() {
+        PiecePositionSystem pps = init();
+
+        try {
+            pps.getPositionDetails().clear();
+            assertEquals("No Exception was thrown when trying to modify list,", 1, 1);
+        } catch (UnsupportedOperationException ex) {}
+    }
+
+    /**
+     * Check the position.
+     */
+    private void checkXYPosition(PiecePositionSystem pps, int row, int index, int x, int y) {
+        PieceDrawingDetails d = pps.get(createPosition(row, index));
+
+        assertTrue("(X,Y) not correct.", d.getX() == x && d.getY() == y);
+    }
+
+    /**
+     * Test that the calculations create the correct XY Calculations
+     */
+    public void testPositionXyCalculation() {
+        PiecePositionSystem pps = init();
+
+        checkXYPosition(pps, 0, 0, 600, 215);
+        checkXYPosition(pps, 9, 3, 474, 869);
+        checkXYPosition(pps, 16, 0, 600, 1378);
     }
 }
