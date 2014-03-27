194e9c0b0b7b6f3c65c7715137417cfb78ccb3d0
Peter Pobojewski <peterpobojewski@gmail.com>
2014-03-27 14:53:53 -0400

    Changed fillGame to be instead AiJoinGame. Now takes a gameID and returns a count of players in that game. Should be used to fill a game with AI users if the matchmaking period expires.

--- server/server-application/database/CCDB.sql
+++ server/server-application/database/CCDB.sql
@@ -36,16 +36,6 @@ CREATE TABLE `games` (
   CONSTRAINT `winnerlink` FOREIGN KEY (`winnerID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `games`
---
-
-LOCK TABLES `games` WRITE;
-/*!40000 ALTER TABLE `games` DISABLE KEYS */;
-INSERT INTO `games` VALUES (6,2,1,1,'2014-03-12 14:57:07',1),(7,2,1,4,'2014-03-12 14:59:34',NULL);
-/*!40000 ALTER TABLE `games` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -114,16 +104,6 @@ CREATE TABLE `gamesusers` (
   CONSTRAINT `userlink` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gamesusers`
---
-
-LOCK TABLES `gamesusers` WRITE;
-/*!40000 ALTER TABLE `gamesusers` DISABLE KEYS */;
-INSERT INTO `gamesusers` VALUES (18,1,6,'Pete',1),(19,6,6,'Tim',4),(20,3,7,'Tim',1),(21,5,7,'Anonymous5',4);
-/*!40000 ALTER TABLE `gamesusers` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -214,16 +194,6 @@ CREATE TABLE `lookuptable` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lookuptable`
---
-
-LOCK TABLES `lookuptable` WRITE;
-/*!40000 ALTER TABLE `lookuptable` DISABLE KEYS */;
-INSERT INTO `lookuptable` VALUES (1,0,0),(1,1,0),(1,1,1),(1,2,0),(1,2,1),(1,2,2),(1,3,0),(1,3,1),(1,3,2),(1,3,3),(2,4,9),(2,4,10),(2,4,11),(2,4,12),(2,5,9),(2,5,10),(2,5,11),(2,6,9),(2,6,10),(2,7,9),(3,9,9),(3,10,9),(3,10,10),(3,11,9),(3,11,10),(3,11,11),(3,12,9),(3,12,10),(3,12,11),(3,12,12),(4,13,0),(4,13,1),(4,13,2),(4,13,3),(4,14,0),(4,14,1),(4,14,2),(4,15,0),(4,15,1),(4,16,0),(5,9,0),(5,10,0),(5,10,1),(5,11,0),(5,11,1),(5,11,2),(5,12,0),(5,12,1),(5,12,2),(5,12,3),(6,4,0),(6,4,1),(6,4,2),(6,4,3),(6,5,0),(6,5,1),(6,5,2),(6,6,0),(6,6,1),(6,7,0);
-/*!40000 ALTER TABLE `lookuptable` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `pieces`
 --
 
@@ -239,16 +209,6 @@ CREATE TABLE `pieces` (
   CONSTRAINT `usersgameslink` FOREIGN KEY (`guID`) REFERENCES `gamesusers` (`guID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pieces`
---
-
-LOCK TABLES `pieces` WRITE;
-/*!40000 ALTER TABLE `pieces` DISABLE KEYS */;
-INSERT INTO `pieces` VALUES (21,1,0),(21,1,1),(21,2,0),(21,2,1),(21,2,3),(21,3,0),(21,3,1),(21,3,3),(21,3,4),(21,5,5);
-/*!40000 ALTER TABLE `pieces` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -319,18 +279,29 @@ CREATE TABLE `users` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Peter',0),(3,'Tim',0),(5,'Anonymous5',1),(6,'Anonymous6',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Dumping routines for database 'ccdb'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `AiJoinGame` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AiJoinGame`(IN gID INT)
+BEGIN
+	CALL `createAI`(@aiID);
+	CALL `userJoinGame`(@aiID, gID);
+	SELECT COUNT(userID) from gamesusers WHERE gameID = gID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `checkWinner` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -470,34 +441,6 @@ BEGIN
         SET MESSAGE_TEXT = 'AI user detected, deletion aborted';
 	END IF;
 	
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `fillGame` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `fillGame`(IN gID INT)
-BEGIN
-	CALL `getNumberOfPlayers`(gID, @total);
-	CALL `countPlayers`(gID, @num);
-	filler: LOOP
-		IF (@num = 1) THEN
-			LEAVE filler;
-		END IF;
-		CALL `createAI`(@aiID);
-		CALL `userJoinGame`(@aiID, gID);
-		SET @num = @num+1;
-	END LOOP filler;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -945,4 +888,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-03-23 17:42:26
+-- Dump completed on 2014-03-27 14:52:12
