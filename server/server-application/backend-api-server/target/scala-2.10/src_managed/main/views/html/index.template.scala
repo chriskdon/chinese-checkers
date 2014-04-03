876166a238d1535fe5c7e3531434c1253e01bb82
kubasub <kubasub12@gmail.com>
2014-04-02 21:28:10 -0400

    fixed bug with showing the number of players in a lsit

--- server/server-application/backend-api-server/target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ server/server-application/backend-api-server/target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -38,8 +38,8 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Wed Apr 02 18:10:49 EDT 2014
-                    SOURCE: /Users/chriskellendonk/Development/School/COSC3F00/chinese-checkers/server/server-application/backend-api-server/app/views/index.scala.html
+                    DATE: Wed Apr 02 20:43:27 EDT 2014
+                    SOURCE: /Users/kubasub/Dropbox/Code/chinese-checkers/server/server-application/backend-api-server/app/views/index.scala.html
                     HASH: 080fc198deedd12cb8158e301d69b93c2ab76a30
                     MATRIX: 556->1|667->18|704->21|735->44|774->46|815->53|829->59|867->76|900->79
                     LINES: 19->1|22->1|24->3|24->3|24->3|26->5|26->5|26->5|28->7
